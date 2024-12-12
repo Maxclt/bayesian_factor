0a4fc8158da897c157420d844057a504e92da9cd
mcoulet <m.coulet@quant-cube.com>
2024-12-12 20:03:41 +0100

    added comments

--- src/sampling/SpSlNormalBayesianFactorGibbs.py
+++ src/sampling/SpSlNormalBayesianFactorGibbs.py
@@ -1,4 +1,12 @@
 import numpy as np
 from scipy.stats import norm, bernoulli, invgamma
+import matplotlib as plt
 
 from src.utils.probability.density import truncated_beta
+
+
+class SpSlNormalBayesianFactorGibbs:
+    def __init__(
+        self, Y, num_factor, alpha, eta, epsilon, lambda_0, lambda_1, burn_in=50
+    ):
+        self.Y = Y
--- src/utils/probability/density.py
+++ src/utils/probability/density.py
@@ -3,12 +3,45 @@ from scipy import stats
 
 
 class truncated_beta(stats.rv_continuous):
-    def _pdf(self, x, alpha, beta, a, b):
+    """Contains methods to sample from a truncated beta distribution
+
+    Args:
+        stats (_type_): scipy.stats class
+    """
+
+    def _pdf(self, x: float, alpha: float, beta: float, a: float, b: float) -> float:
+        """Return the value of a truncated beta(alpha, beta) density function
+        of support [a,b] at x
+
+        Args:
+            x (float): float between 0 and 1
+            alpha (float): shape parameter
+            beta (float): shape parameter
+            a (float): lower bound between 0 and 1
+            b (float): upper bound between 0 and 1
+
+        Returns:
+            float: value of a truncated beta density function at x
+        """
         return stats.beta.pdf(x, alpha, beta) / (
             stats.beta.cdf(b, alpha, beta) - stats.beta.cdf(a, alpha, beta)
         )
 
-    def _cdf(self, x, alpha, beta, a, b):
+    def _cdf(self, x, alpha, beta, a, b) -> float:
+        """Return the value of a truncated beta(alpha, beta) cumulative
+        distribution function of support [a,b] at x
+
+        Args:
+            x (float): float between 0 and 1
+            alpha (float): shape parameter
+            beta (float): shape parameter
+            a (float): lower bound between 0 and 1
+            b (float): upper bound between 0 and 1
+
+        Returns:
+            float: value of a truncated beta cumulative distribution function
+            at x
+        """
         return (stats.beta.cdf(x, alpha, beta) - stats.beta.cdf(a, alpha, beta)) / (
             stats.beta.cdf(b, alpha, beta) - stats.beta.cdf(a, alpha, beta)
         )
